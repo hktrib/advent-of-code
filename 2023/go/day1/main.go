b27ae1764e2ecbe27d3158e8c28de93f4e767be5
Apramay Singh <hktribunal@gmail.com>
2023-12-19 13:05:03 -0800

    [go | day1] Part1 completed

--- 2023/go/day1/main.go
+++ 2023/go/day1/main.go
@@ -1,8 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+var lineLen int = 4096
 
 func main() {
 
-	fmt.Println("Starting aoc-2")
+	file, err := os.Open("../../input/day34.txt")
+	if err != nil {
+		log.Fatalf("Error opening input file: %v", err)
+	}
+	defer file.Close()
+
+	// Setting scanner.Buffer
+	scanner := bufio.NewScanner(file)
+	buffer := make([]byte, lineLen)
+	scanner.Buffer(buffer, lineLen)
+
+	calibrationsSum := 0
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		seenFirst := false
+
+		var snum1 string
+		var snum2 string
+
+		for _, r := range scanner.Text() {
+			if unicode.IsNumber(r) {
+				if !seenFirst {
+					snum1 = string(r)
+					seenFirst = true
+				} else {
+					snum2 = string(r)
+				}
+			}
+
+		}
+
+		var snum string
+
+		if snum1 == "" {
+			// No calibrations in this line
+			continue
+		} else if snum2 == "" {
+			// No second calibration value -> duplicating calibration val 1
+			snum = snum1 + snum1
+		} else {
+			snum = snum1 + snum2
+		}
+
+		calibrationVal, err := strconv.Atoi(snum)
+		if err != nil {
+			log.Fatalf("Unable to convert snum %v to int: %v", snum, err)
+		}
+		calibrationsSum += calibrationVal
+	}
+
+	fmt.Printf("Total Calibrations: %d\n", calibrationsSum)
 }
